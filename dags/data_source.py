7431cea759d08903a479bdcee381df991dc27588
Lobobranco <rntt2511@gmail.com>
2025-01-31 21:37:40 -0300

    include e data_source code

--- dags/data_source.py
+++ dags/data_source.py
@@ -13,19 +13,16 @@ from python.ml_data_source import coletar_dados_ml
     default_args={"owner": "lobobranco", "retries": 3},
     tags=["build a data source"],
 )
-
-BUCKET_NAME = "lobobranco-datalake"
-GCS_PATH = "raw"
-
 def data_source():
 
     init = EmptyOperator(task_id="inicio")
     finish = EmptyOperator(task_id="fim_pipeline")
 
     # Função para coletar os dados de web scraping dos produtos
-    @task
     def extract_mercado_livre_product(produtos_coletados):
 
+        BUCKET_NAME = "lobobranco-datalake"
+        GCS_PATH = "raw"
         for produto in produtos_coletados:
             produto_nome = produto['produto'].replace(" ", "-")
 
@@ -36,9 +33,10 @@ def data_source():
     # Função para verificar se há produtos novos
     @task
     def check_for_new_products():
-        url = 'http://localhost:5000/api/produtos/'  # URL da API Flask
+        url = 'http://172.19.0.2:5000/api/produtos'  # URL da API Flask
 
         try:
+            print("coletando")
             response = requests.get(url)
             if response.status_code == 200:
                 produtos_coletados = response.json()
@@ -56,12 +54,12 @@ def data_source():
     # Configuração do HttpSensor para verificar se há novos produtos
     sensor = HttpSensor(
         task_id="sensor_produtos_novos",
-        http_conn_id="http_default",  # Esse ID deve ser configurado no Airflow Connections
-        endpoint="api/produtos/",
-        poke_interval=600,  # Intervalo em segundos (600s = 10 minutos)
-        timeout=600,  # Tempo máximo para aguardar o produto
-        mode="poke",  # Usando o modo "poke", que verifica a condição a cada intervalo
-        retries=5,  # Quantas vezes o sensor irá tentar
+        http_conn_id="http_default",
+        endpoint="api/produtos",  # Sem barra no início
+        poke_interval=10,
+        timeout=600,
+        mode="poke",
+        retries=5,
     )
 
     # Definição do fluxo de execução das tasks
