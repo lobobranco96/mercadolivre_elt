bda734880af43bac38f052fe983e5609a202ebc2
Lobobranco <rntt2511@gmail.com>
2025-02-12 14:32:04 -0300

    feat: mercado_livre_pipeline dag

--- dags/mercado_livre_pipeline.py
+++ dags/mercado_livre_pipeline.py
@@ -5,28 +5,29 @@ import logging
 from python.mercado_livre import coletar_dados_produtos
 import requests
 import os
+import time
 
 from airflow.decorators import dag, task
 from airflow.operators.empty import EmptyOperator
 from airflow.providers.http.sensors.http import HttpSensor
-from airflow.operators.bash import BashOperator
+from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
+from airflow.operators.python import PythonOperator
 
 from astro import sql as aql
 from astro.files import File
 from astro.sql.table import Table
 
 from cosmos import DbtTaskGroup, ProfileConfig, ProjectConfig
-from google.cloud import storage
-
 
 DBT_PATH = "/usr/local/airflow/dags/dbt/"
 DBT_PROFILE = "dbt_project"
 DBT_TARGETS = "dev"
-os.environ["DBT_PROFILES_DIR"] = "/usr/local/airflow/dags/dbt"
+os.environ["DBT_PROFILES_DIR"] = DBT_PATH
 
 HOJE = pendulum.now().format('YYYY-MM-DD')
 GCP_CONN = "gcp_default"
 BUCKET_NAME = "mercado-livre-datalake"
+PREFIX = f"raw/{HOJE}"
 
 profile_config = ProfileConfig(
     profile_name=DBT_PROFILE,
@@ -45,7 +46,6 @@ default_args = {
     "retry_delay": 0
 }
 
-# Configuração do logger
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -55,19 +55,17 @@ logger = logging.getLogger(__name__)
 
 @dag(
     start_date=datetime(2024, 1, 1),
-    schedule="@daily",  # Ajuste o schedule conforme necessário
+    schedule="@daily",
     catchup=False,
     doc_md=__doc__,
     default_args=default_args,
     tags=["build a data source"],
 )
 def pipeline():
-
     init = EmptyOperator(task_id="inicio")
     finish = EmptyOperator(task_id="fim_pipeline")
 
     endpoint_api = f"/api/produtos/date/{HOJE}"
-    # Configuração do HttpSensor para verificar se há novos produtos
     sensor = HttpSensor(
         task_id="sensor_produtos_novos",
         http_conn_id="http_default", 
@@ -81,60 +79,78 @@ def pipeline():
     def buscar_produtos_api():
         url = f'http://host.docker.internal:5000/api/produtos/date/{HOJE}'
         response = requests.get(url)
-        if response.status_code == 200 and len(response.json()) > 0:
-            return response.json()  # Retorna a lista de produtos
+        if response.status_code == 200 and response.json():
+            return response.json()
         else:
             logger.info("Nenhum produto encontrado ou erro na requisição.")
             return []
 
-    # Função que processa cada produto
     def coleta_produto(produto):
         gcs_path = "raw"
         try:
             logger.info('Iniciando a coleta de produtos...')
             nome_produto = produto['produto'].replace(" ", "-")
             url_produto = f"https://lista.mercadolivre.com.br/{nome_produto}"
-            data_insercao = produto['data_insercao']  # Ex. 2025-01-25
+            data_insercao = produto['data_insercao']
             gcs_full_path = f"{gcs_path}/{data_insercao}/{nome_produto}.csv"
-
-            # Coleta os dados e armazena no GCS
             coletar_dados_produtos(url_produto, BUCKET_NAME, gcs_full_path)
         except requests.exceptions.RequestException as e:
             logger.error(f"Erro ao coletar dados: {e}")
 
-    # Função para iniciar a coleta de dados dos produtos
     @task(task_id="iniciar_mercado_livre")
     def iniciar_mercado_livre():
         produtos = buscar_produtos_api()
-        if produtos:  # Verifica se há produtos antes de coletar
+        if produtos:
             for produto in produtos:
                 coleta_produto(produto)
+                time.sleep(1500) # 25minutos para iniciar a coleta do proximo produto
         else:
             logger.info("Nenhum produto encontrado para coleta.")
 
     ml_extract = iniciar_mercado_livre()
 
-    gcs_data_path = f"gs://{BUCKET_NAME}/raw/{HOJE}/tenis-masculino.csv"
-    gcs_to_bigquery_task = aql.load_file(
-            task_id="product_data",
-            input_file=File(path=gcs_data_path,conn_id=GCP_CONN),
-            output_table=Table(
-        name="produtos",
-        conn_id=GCP_CONN,
-        metadata={"schema": "mercadolivre", "database": "projeto-lobobranco"}),
-            use_native_support=False,
-            columns_names_capitalization="original",
-            if_exists="append"
-        )
-  
+    def processar_arquivos(**kwargs):
+        ti = kwargs['ti']
+        arquivos = ti.xcom_pull(task_ids="listar_arquivos")
+        if not arquivos:
+            logger.info("Nenhum arquivo encontrado para processamento.")
+            return
+
+        for arquivo in arquivos:
+            gcs_data_path = f"gs://{BUCKET_NAME}/{arquivo}"
+            aql.load_file(
+                task_id=f"load_{arquivo.replace('/', '_')}",
+                input_file=File(path=gcs_data_path, conn_id=GCP_CONN),
+                output_table=Table(
+                    name="produtos",
+                    conn_id=GCP_CONN,
+                    metadata={"schema": "mercadolivre", "database": "projeto-lobobranco"}
+                ),
+                use_native_support=False,
+                columns_names_capitalization="original",
+                if_exists="append"
+            )
+            logger.info(f"Processando arquivo: {arquivo}")
+
+    listar_arquivos = GCSListObjectsOperator(
+        task_id="listar_arquivos",
+        bucket=BUCKET_NAME,
+        prefix=PREFIX,
+        gcp_conn_id=GCP_CONN,
+        do_xcom_push=True
+    )
+
+    gcs_to_bigquery = PythonOperator(
+        task_id="processar_arquivos",
+        python_callable=processar_arquivos
+    )
+
     dbt_running_models = DbtTaskGroup(
         group_id="dbt_running_models",
         project_config=project_config,
-        profile_config=profile_config,
-        default_args={"retries": 2},
+        profile_config=profile_config
     )
 
-    # Definindo o fluxo de execução
-    init >> sensor >> ml_extract >> gcs_to_bigquery_task >> dbt_running_models >> finish
+    init >> sensor >> ml_extract >> listar_arquivos >> gcs_to_bigquery >> dbt_running_models >> finish
 
 data_source = pipeline()
